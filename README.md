cafc3c73e9557cf49dc032135ae106624726ae48
Bowald <j_bowald@hotmail.com>
2014-03-16 19:22:17 +0100

    Script is now Dynamic! Add users in sender.txt, Also the code is cleaned up and the script is easier to use.

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
-snd-anteckningar
+snd-anteckningar by Johan Bowald
 ================
 
 Python script that fetches jpg's or a pdf from a mail server and merges every containing several jpgs to a pdf/mail.
 
+========================= SET UP =========================
+
+sender.txt contains the contributers.
+They are the ony ones that are able to upload files via email.
+
+password.txt
+should contain the base64 encryption of your password
 
 ========================= RUNNING DETAILS =========================
-The script is design to work with the courses ERE102, TMV216 and TDA
-
 You'll need Imagemagick and python
 
 Imagemagick download: http://www.imagemagick.org/script/index.php
 
-The scrippt will first set up the folder structure needed.
-Then Download the attachments, compress them to a .pdf and place in "uppladdat" directory.
+The script will first set up the folder structure needed.
+Then Download the attachments, compress them to a .pdf and place in "www/uppladdat" directory.
 
-When all new emails are downloaded, an .html-file will be created with links to every downloaded file.
+When all new emails are downloaded, an .html-file will be created with links to every downloaded file. Sorted by the course-code specified in sender.txt
+
+========================= TODO! =========================
+
+Need a new design on the HTML page. The boxes needs to expand downwards or a menu would need to be added. And only show one box at the time
+
+Add Connection constants, so any email can be used without changing the python file.
+
+========================= Bugs! =========================
+
+The scipt does not send an email to people that isn't in the sender.txt list
--- main.py
+++ main.py
@@ -13,23 +13,19 @@ import shutil
 import base64
 from time import gmtime, strftime
 
-def init_script():
-    if not os.path.exists("uppladdat"):
-        os.makedirs("uppladdat")
-        os.makedirs("uppladdat/ERE102")
-        os.makedirs("uppladdat/TIF085")
-        os.makedirs("uppladdat/TMV216")
-        os.makedirs("uppladdat/unsorted")
-    else:
-        if not os.path.exists("uppladdat/ERE102"):
-            os.makedirs("uppladdat/ERE102")
-        if not os.path.exists("uppladdat/TIF085"):
-            os.makedirs("uppladdat/TIF085")
-        if not os.path.exists("uppladdat/TMV216"):
-            os.makedirs("uppladdat/TMV216")
-        if not os.path.exists("uppladdat/unsorted"):
-            os.makedirs("uppladdat/unsorted")
 
+#Creates a dir for every course in sender.txt
+def init_script():
+    f = open('sender.txt')
+    lines = f.readlines()
+    f.close()
+    for emailAddress in lines :
+        splitted = re.split(r'\s+', emailAddress.rstrip('\t\n'))
+        if not splitted[0][:1] == '#' :
+            if not os.path.exists("www/uppladdat/{0}".format(splitted[1])):
+                os.makedirs("www/uppladdat/{0}".format(splitted[1]))
+
+#Fetches the email attachment, and complise multiple .jpg's to a .pdf
 def fetch_and_store():
     detach_dir = '.'
     m = imaplib.IMAP4_SSL("imap.gmail.com")
@@ -55,10 +51,15 @@ def fetch_and_store():
         temp = m.store(emailid,'+FLAGS', '\\Seen')
         m.expunge()
 
-        if not parseSubLine(subjectline) :             #Ifall mailet har fel rubrik, går vi in här
-           print "send mail"
-           # sendEmail(name, fromaddr, subjectline)      #Skickar ett mail till avsändaren om att dens mail haft fel format på rubriken
-        else:   
+        if not parseSubLine(subjectline) and checkSender(fromaddr) :             #Ifall mailet har fel rubrik, går vi in här
+            if not parseSubLine(subjectline) :
+                print "wrong subjectline"
+                sendEmail(name, fromaddr, subjectline, "2")
+            elif not checkSender(fromaddr) :
+                print "address does not exists"
+                sendEmail(name, fromaddr, subjectline, "1")      #Skickar ett mail till avsändaren om att dens mail haft fel format på rubriken
+            print checkSender(fromaddr)
+        else:
             if not os.path.exists(subjectline):
                 os.makedirs(subjectline)
 
@@ -81,60 +82,83 @@ def fetch_and_store():
                     fp.write(part.get_payload(decode=True))
                     fp.close()
             
+            var = checkSender(fromaddr)
+            course = var[0]
+            name = var[1]
+            dest = "www/uppladdat/" + course + "/{0}.pdf".format(setFileName(name,subjectline))
             dirs = os.listdir(format(subjectline))
-            subdir = "unsorted"
-            if (subjectline.startswith("ERE102-") or subjectline.startswith("TIF085-") or subjectline.startswith("TMV216-")) :
-               subdir = subjectline[:6]
             if dirs[0].endswith(".jpg") :
-               call(["convert"] + glob.glob(subjectline + "/*") + ["uppladdat/" + subdir + "/{0}.pdf".format(subjectline)])
+               call(["convert"] + glob.glob(subjectline + "/*") + [dest])
                shutil.rmtree(subjectline)
             elif dirs[0].endswith(".pdf") and len(dirs) == 1 :
                path = subjectline + "/" + dirs[0]
-               dest = "uppladdat/" + subdir + "/{0}.pdf".format(subjectline)
                shutil.move(path,dest)
                shutil.rmtree(subjectline)
-            #elif dirs[0].endswith(".pdf") :
-               #print ["pdftk"] + glob.glob(subjectline + "/*") + ["cat output uppladdat/" + "{0}.pdf".format(subjectline)]
-               #call(["pdftk"] + glob.glob(subjectline + "/*") + ["cat output uppladdat/" + "{0}.pdf".format(subjectline)])
-               #shutil.rmtree(subjectline) #Hämtar alla attechments, sätter ihop de till en .pdf
 
+# Creates a HTML-Page
 def createHTML():
-    uppladdat = os.listdir("uppladdat")
-    f = open("index.html", "w")
+    f = open("www/index.html", "w")
     f.write('<html><head><title>Anteckningar Dtek</title><meta charset="UTF-8"><link href="mall.css" rel="stylesheet" media="all"></head><body>')
     f.write('<div class="flex-container flex-container-style fixed-height">')
-    f.write('<div class="flex-item"><h1> Reglerteknik </h1><ul id="ERE102">')
-    for fil in os.listdir("uppladdat/ERE102") :
-        if(fil.endswith(".pdf")) :
-            f.write('<li><a href="uppladdat/ERE102/{0}">{1}</a></li>'.format(fil,fil[7:]))
-    f.write('</ul></div>')
-    f.write('<div class="flex-item"><h1> Fysik för ingenjörer</h1><ul id="TIF085"> ')
-    for fil in os.listdir("uppladdat/TIF085") :
-        if(fil.endswith(".pdf")) :
-            f.write('<li><a href="uppladdat/TIF085/{0}">{1}</a></li>'.format(fil,fil[7:]))
-    f.write('</ul></div>')
-    f.write('<div class="flex-item"> <h1> Linjär algebra</h1><ul id="TMV216">')
-    for fil in os.listdir("uppladdat/TMV216") :
-        if(fil.endswith(".pdf")) :
-            f.write('<li><a href="uppladdat/TMV216/{0}">{1}</a></li>'.format(fil,fil[7:]))
-    f.write('</ul></div></div>')
+    f.write( listFileHTML() )
+    f.write('</div>')
     f.write('<div class="disc"><a href="http://snd.dtek.se"><img src="http://snd.dtek.se/wp-content/uploads/2013/02/sndlogga.png"></a>')
     f.write('<div class="discText"> Disclaimer: På denna sidan samlas anteckningar gjorda av Teknologer på D-sektionen. Vi i SND kan ej garantera att anteckningarna stämmer.</div>')
     f.write('<div class="discText"> Uppdaterad: ' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '</div></div>')
     f.write('</body></html>') #Bygger html-sidan
 
-def parseSubLine(string): #Parsar rubriken, kollar om det är rätt format
-    splitted = string.split("-")
-    if not (len(splitted) == 5 or len(splitted) == 6):
-        return False
-    if (re.match("[A-Za-z]", splitted[0][:3]) and re.match("[0-9]", splitted[0][-3:])):
-        if (re.match("[0-9]",splitted[1]) and re.match("[0-9]",splitted[2]) and re.match("[0-9]",splitted[3])):
-            return True
+def listFileHTML():
+    string = ""
+    for dir in sortDir(os.listdir("www/uppladdat")):
+        if os.path.isdir("www/uppladdat/{0}".format(dir)):
+            string += str('<div class="flex-item"><h1>{0}</h1><ul id="{0}">'.format(dir))
+            for fil in sortDir(os.listdir("www/uppladdat/{0}".format(dir))) :
+                if fil.endswith(".pdf") :
+                    string += str('<li><a href="uppladdat/{0}/{1}">{2}</a></li>'.format(fil[:6],fil,fil[7:]))
+            string += str('</ul></div>')
+    return string
+
+def checkSender(string) :
+    f = open('sender.txt')
+    lines = f.readlines()
+    f.close()
+    for emailAddress in lines :
+        splitted = re.split(r'\t+', emailAddress.rstrip('\t\n'))
+        if not splitted[0] == '#' :
+            if splitted[0] == string:
+                return splitted[1:]
     return False
 
-def sendEmail(name,toaddrs,subjectline): # Skickar ett mail
+def sortDir(dir) :
+    return sorted(dir, key=lambda x: x.split('-')[1:4])
+
+def parseSubLine(string):
+    splitted = string.split("-")
+    if not (len(splitted) == 2 or len(splitted) == 3):
+        return False
+    if len(splitted[0]) > 2 or len(splitted[1]) > 2 :
+        return False
+    if re.match("[0-9]",splitted[0]) and re.match("[0-9]",splitted[1]) :
+        return True
+    return False
+
+def setFileName(name,subjectline) :
+    splitted = subjectline.split("-")
+    year = strftime("%Y", gmtime())
+    date = "{0}-{1}".format(splitted[0],splitted[1])
+    info = ""
+    if len(splitted) > 2:
+        info = "-{0}".format(splitted[2])
+    return "EEM076-{0}-{1}-{2}{3}".format(year,date,name,info)
+
+# Skickar ett mail om
+def sendEmail(name,toaddrs,subjectline,error): 
     fromaddr = 'dtekanteckningar@gmail.com'
-    msg = 'Hej ' + name +'\n \n Det mail du har skickat med rubriken ' + subjectline + ' har fel format. Formatet är ERE102-2013-MM-DD-CID. Vänligen skicka ett nytt mail.\n\n MVH SND' 
+    if error == "1" :
+        errorMsg = "Detta beror på att denna mail ej är registrerad som antecknare. Hör av dig till snd@dtek.se så fixar dem det."
+    elif error == "2" :
+        errorMsg = "Rubriken: {0} på ditt mail har fel format. Formatet skall vara MM-DD exempel 03-12 för 12 Mars. Skicka ett nytt mail med rätt rubrik :)".format(subjectline)
+    msg = 'Hej {0}\n\n Uppladdningen av dina antecknignar gick fel.\n{1}\n\n MVH SND'.format(name,errorMsg) 
     sub = "Uppladdning av dina anteckingar gick fel"
  
     username = 'dtekanteckningar'  
@@ -146,6 +170,7 @@ def sendEmail(name,toaddrs,subjectline): # Skickar ett mail
     server.sendmail(fromaddr, toaddrs, message)  
     server.quit()  
 
+#Hämtar lösenordet till gmail-kontot
 def getPassword():
     password = ""
     f = open('password.txt', 'r')
@@ -153,7 +178,7 @@ def getPassword():
         password = f.read()
     finally:
         f.close()
-    return base64.b64decode(password) #Hämtar lösenordet till gmail-kontot
+    return base64.b64decode(password) 
 
 
 #### Här Börjar Programet ####
